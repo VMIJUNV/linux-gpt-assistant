cd6761378de8e397183f8284324f02a5b579afe9
VMIJUNV <110038507+VMIJUNV@users.noreply.github.com>
2023-04-05 10:22:30 +0800

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"  # 你的 OpenAI API Key
+openai.api_key = ""  # 你的 OpenAI API Key
 
 def getgpt(msg):
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
